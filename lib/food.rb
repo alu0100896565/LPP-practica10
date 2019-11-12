ffdb1e5ee5dca993e93ea15a2991a396bcb6b11a
Eduardo Suarez Ojeda <alu0100896565@ull.edu.es>
2019-11-12 07:47:50 +0000

    Existe un atributo con las emisiones

--- lib/food.rb
+++ lib/food.rb
@@ -3,10 +3,11 @@
 
   class Alimentos
 
-    attr_reader :nombre
-    def initialize(nombre)
+    attr_reader :nombre, :emisiones
+    def initialize(nombre, emisiones)
 
       @nombre = nombre
+      @emisiones = emisiones
 
     end
   end
--- spec/food_spec.rb
+++ spec/food_spec.rb
@@ -1,7 +1,7 @@
 require 'food'
 RSpec.describe Alimentos do
   before (:all) do
-    @lentejas = Alimentos.new("lentejas")
+    @lentejas = Alimentos.new("lentejas", 0.4)
   end
 
   context "Probando la clase Alimentos" do
