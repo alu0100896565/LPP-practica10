9085f7fc7322056e2bcc06e65f5c4d839b4cef40
Eduardo Suarez Ojeda <alu0100896565@ull.edu.es>
2019-11-13 16:22:55 +0000

    Añadiendo ejempos de dietas carnivora y vegana para comparar datos

--- lib/food.rb
+++ lib/food.rb
@@ -21,11 +21,11 @@
     end
 
     def valorEnergetico
-      (@prot*4.0)+(@carbs*4.0)+(@lip*9.0)
+      ((@prot*4.0)+(@carbs*4.0)+(@lip*9.0)).round(3)
     end
 
     def +(other)
-      suma = { :nombre => "Mezcla", :emisiones => self.emisiones+other.emisiones, :terreno => self.terreno+other.terreno, :prot => self.prot+other.prot, :carbs => self.carbs+other.carbs, :lip => self.lip+other.lip}
+      suma = { :nombre => "Mezcla", :emisiones => (self.emisiones+other.emisiones).round(3), :terreno => (self.terreno+other.terreno).round(3), :prot => (self.prot+other.prot).round(3), :carbs => (self.carbs+other.carbs).round(3), :lip => (self.lip+other.lip).round(3)}
       Alimentos.new(suma)
     end
   end
@@ -61,7 +61,7 @@
 
     def impactoAmbiental
 
-      "Para la dieta Dietita las emisiones de gases kgCO2eq son: #{@alimentoTotal.emisiones} y el terreno usado en m2 por año es: #{@alimentoTotal.terreno}"
+      "Para la dieta #{@nombre} las emisiones de gases kgCO2eq son: #{@alimentoTotal.emisiones} y el terreno usado en m2 por año es: #{@alimentoTotal.terreno}"
 
     end
     
