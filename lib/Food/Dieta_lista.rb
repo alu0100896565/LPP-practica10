39af5eeb940b7f4c6d7d89388a189e8cf4c0d21b
Eduardo Suarez Ojeda <alu0100896565@ull.edu.es>
2019-11-19 19:20:42 +0000

    La clase Dieta_lista admite una lista como parametro del new

--- lib/Food/Dieta_lista.rb
+++ lib/Food/Dieta_lista.rb
@@ -2,7 +2,10 @@ module Food
 
     class Dieta_lista
 
-       def initialize
+       def initialize(lista)
+
+        raise TypeError, "Se espera como argumento una lista: List" unless lista.is_a?List
+        @lista_alim = lista
 
     end
 
