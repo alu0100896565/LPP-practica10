c471d70d9732d89970067c204a6ef192351f486a
Eduardo Suarez Ojeda <alu0100896565@ull.edu.es>
2019-11-24 13:18:24 +0000

    Los platos tienen una lista de cantidades de alimento en gramos

--- lib/Food/Platos.rb
+++ lib/Food/Platos.rb
@@ -1,10 +1,12 @@
 module Food
 
 class Platos
-attr_reader :nombre, :ali_list
+attr_reader :nombre, :ali_list, :gram_list
 def initialize(nombre)
     @nombre = nombre
     @ali_list = List.new
+    @gram_list = List.new
+
 end
 
 end
