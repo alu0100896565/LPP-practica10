fe99dffbde893c0dceab3c52a25ce169374f2f62
Eduardo Suarez Ojeda <alu0100896565@ull.edu.es>
2019-11-26 08:46:14 +0000

    Se obtiene la eficiencia energetica formateada

--- lib/Food/Platos_Ambiental.rb
+++ lib/Food/Platos_Ambiental.rb
@@ -14,7 +14,13 @@ module Food
         (Platos.instance_method(:alim_total).bind(self).call).terreno
     end
 
-    
+    def efiEn
+        x = "Para crear el plato: #{@nombre}, se generan "
+        x += (Platos.instance_method(:alim_total).bind(self).call).emisiones.to_s
+        x+= " de emisiones de CO2 y "
+        x += (Platos.instance_method(:alim_total).bind(self).call).terreno.to_s
+        x += " de metros cuadrados de terreno"
+    end
 
     end
 end
