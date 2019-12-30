71d9011e61c0adbc119efcf03294b5c9da89af07
Eduardo Suarez Ojeda <alu0100896565@ull.edu.es>
2019-12-30 13:17:24 +0000

    La clase menu puede añadir platos y sus precios

--- lib/Food/Menu.rb
+++ lib/Food/Menu.rb
@@ -13,6 +13,16 @@ module Food
             Platos.arrayPlatos.find { |x| name == x.descripcion }
         end
 
+        def componente(options = {})
+            if options[:descripcion]
+                @conjPlatos << findPlato(options[:descripcion])
+                precio = 9.99
+                precio = options[:precio] if options[:precio]
+                @conjPrecios << precio
+            else return nil
+            end
+        end
+
     end
 
 end
