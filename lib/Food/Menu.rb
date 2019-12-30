4a4103e13c886500782e5bd4bc55320910326fd2
Eduardo Suarez Ojeda <alu0100896565@ull.edu.es>
2019-12-30 10:19:15 +0000

    La clase menu tiene nombre y descripcion

--- lib/Food/Menu.rb
+++ lib/Food/Menu.rb
@@ -1,7 +1,11 @@
 module Food
 
     class Menu
-
+        attr_reader :nombre, :descripcion
+        def initialize(nombre)
+            @nombre = nombre
+            @descripcion = ""
+        end
 
     end
 
